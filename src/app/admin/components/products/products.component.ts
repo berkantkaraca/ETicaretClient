58a2dd6172368013f0c1c06fa7a1eea9043e6642
Berkant Karaca <berkantkaracatr@gmail.com>
2025-12-21 11:49:25 +0300

    Ürün Ekleme İşlemleri - ProductServce

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -16,41 +15,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
-
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
-
-    // this.httpClientService.post({
-    //   controller: "products"
-    // }, {
-    //   "name": "Kalem",
-    //   "stock": 100,
-    //   "price": 15
-    // }).subscribe();
-
-    //   this.httpClientService.put({
-    //     controller: "products"
-    //   }, {
-    //     "id": "a567819b-e856-4d72-8ac2-da988b4b5755",
-    //     "name": "Kalemssssssssssssssss",
-    //     "stock": 65100,
-    //     "price": 515
-    //   }).subscribe();
-    // }
-
-    // this.httpClientService.delete({
-    //   controller: "products"
-    // }, "76311db2-498a-4572-8772-a765ab11c20c").subscribe();
-
-    //farklı bir servise doğrudan istek atmak
-    this.httpClientService.get({
-      // baseUrl: "https://jsonplaceholder.typicode.com",
-      // controller: "posts",
-      fullEndPoint: "https://jsonplaceholder.typicode.com/posts"
-    }).subscribe(data => console.log(data));
-
     
   }
 }
