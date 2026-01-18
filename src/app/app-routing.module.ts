40c6b39528c962abbe7c88bff6717ba4bc8a556e
Berkant Karaca <berkantkaracatr@gmail.com>
2026-01-18 22:21:24 +0300

    Kullanıcı Kayıt Formu Tasarlama (Reactive Form)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,8 @@ const routes: Routes = [
   },
   { path: "", component: HomeComponent },
   { path: "baskets", loadChildren: () => import("./ui/components/baskets/baskets.module").then(m => m.BasketsModule) },
-  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(m => m.ProductsModule)}
+  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(m => m.ProductsModule)},
+  { path: "register", loadChildren: () => import("./ui/components/register/register.module").then(m => m.RegisterModule)}
 ];
 
 @NgModule({
