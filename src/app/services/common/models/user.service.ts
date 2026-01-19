76a9e7c52e59039a6edc1d51efd74eef1b60cbb9
Berkant Karaca <berkantkaracatr@gmail.com>
2026-01-19 19:55:15 +0300

    Identity Mekanizmasının Kurulumu ve Kullanıcı Ekleme

---
+++ src/app/services/common/models/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Toast } from 'ngx-toastr';
+import { firstValueFrom, observable, Observable } from 'rxjs';
+import { User } from '../../../entities/user';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
+import { HttpClientService } from '../http-client.service';
+import { Create_User } from 'src/app/contracts/users/create_user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private httpClientService: HttpClientService, private toastrService: CustomToastrService) { }
+
+  async create(user: User): Promise<Create_User> {
+    const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
+      controller: "users"
+    }, user);
+
+    return await firstValueFrom(observable) as Create_User;
+  }
+}
--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControl, UntypedFormBuilder, UntypedFormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent } from 'src/app/base/base.component';
+import { Create_User } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
+import { UserService } from 'src/app/services/common/models/user.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
@@ -14,6 +16,7 @@ export class RegisterComponent extends BaseComponent implements OnInit {
 
   constructor(
     private formBuilder: UntypedFormBuilder,
+    private userService: UserService,
     private toastrService: CustomToastrService,
     spinner: NgxSpinnerService
   ) {
@@ -60,5 +63,18 @@ export class RegisterComponent extends BaseComponent implements OnInit {
 
     if (this.frm.invalid)
       return;
+
+    const result: Create_User = await this.userService.create(user);
+
+    if (result.succeeded)
+      this.toastrService.message(result.message, "Kullanıcı Kaydı Başarılı", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
+    else
+      this.toastrService.message(result.message, "Hata", {
+        messageType: ToastrMessageType.Error,
+        position: ToastrPosition.TopRight
+      })
   }
 }
