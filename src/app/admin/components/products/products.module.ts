053a668477bd8e1808df27f1a40779ccc2100b8b
Berkant Karaca <berkantkaracatr@gmail.com>
2026-02-15 12:30:41 +0300

    Siparişlerin Yönetim Panelinde Listelenmesi

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -10,17 +10,16 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
 import {MatDialogModule} from '@angular/material/dialog';
 import { FileUploadModule } from 'src/app/services/common/file-upload/file-upload.module';
 import { DialogModule } from 'src/app/dialogs/dialog.module';
+import { DeleteDirectiveModule } from 'src/app/directives/admin/delete.directive.module';
 
 @NgModule({
   declarations: [
     ProductsComponent,
     CreateComponent,
-    ListComponent,
-    DeleteDirective
+    ListComponent
   ],
   imports: [
     CommonModule,
@@ -35,7 +34,8 @@ import { DialogModule } from 'src/app/dialogs/dialog.module';
     DialogModule,
     RouterModule.forChild([
       { path: "", component: ProductsComponent }
-    ])
+    ]),
+    DeleteDirectiveModule
   ]
 })
 export class ProductsModule { }
---
+++ src/app/directives/admin/delete.directive.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { DeleteDirective } from './delete.directive';
+
+@NgModule({
+  declarations: [DeleteDirective],
+  imports: [
+    
+  ],
+  exports: [DeleteDirective]
+})
+export class DeleteDirectiveModule { }
